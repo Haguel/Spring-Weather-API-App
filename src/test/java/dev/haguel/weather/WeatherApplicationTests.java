22b5f47429e2409b7b9a5f96d31a309a3f115789
GlebHal <hammanov.gleb@gmail.com>
2023-11-14 16:58:27 +0200

    Made dockerization

--- src/test/java/dev/haguel/weather/WeatherApplicationTests.java
+++ src/test/java/dev/haguel/weather/WeatherApplicationTests.java
@@ -3,10 +3,8 @@ package dev.haguel.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class WeatherApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
